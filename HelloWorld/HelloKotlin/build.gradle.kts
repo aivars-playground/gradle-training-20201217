003c058d4bc858f69469014b7f3480ff4ebababd
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2020-12-17 17:36:30 +0200

    adding code style plugin

--- HelloWorld/HelloKotlin/build.gradle.kts
+++ HelloWorld/HelloKotlin/build.gradle.kts
@@ -4,3 +4,29 @@
  * This is a general purpose Gradle build.
  * Learn more about Gradle by exploring our samples at https://docs.gradle.org/6.7.1/samples
  */
+
+plugins{
+    java
+    id("com.gradleup.static-analysis") version "1.3"
+}
+
+tasks.register("hello") {
+    doFirst {
+        println("first hello")
+    }
+    doLast {
+        println("last hello")
+    }
+}
+
+tasks.register("world") {
+
+    dependsOn("hello")
+
+    doFirst {
+        println("first world")
+    }
+    doLast {
+        println("last world")
+    }
+}
